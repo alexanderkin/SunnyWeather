9f9ffa3b719e679985a156f4aca0f784c35b6975
alexanderkin <211560569@qq.com>
2022-07-07 15:11:42 +0800

    添加基础功能

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -8,6 +8,7 @@ class SunnyWeatherApplication : Application() {
     companion object {
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
+        const val TOKEN = "MUilzIFAIfmMyet1"
     }
 
     override fun onCreate() {
